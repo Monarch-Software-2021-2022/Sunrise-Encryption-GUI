9d442b1a8c34c703dcece4f7b52e223add748225
KuebV <rwthompson01@bvsd.org>
2021-10-11 22:39:34 -0600

    Inital Commit

---
+++ SunriseEncryption/SunriseEncryption/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿
+namespace SunriseEncryption
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SidePanel = new System.Windows.Forms.Panel();
+            this.HorizontalDivider = new System.Windows.Forms.Panel();
+            this.HomeButton = new System.Windows.Forms.Button();
+            this.HashButton = new System.Windows.Forms.Button();
+            this.EncryptButton = new System.Windows.Forms.Button();
+            this.VerticalDivider = new System.Windows.Forms.Panel();
+            this.FileEncryptionButton = new System.Windows.Forms.Button();
+            this.HomePagePanel = new System.Windows.Forms.Panel();
+            this.HashingPagePanel = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SidePanel.SuspendLayout();
+            this.HomePagePanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(166, 142);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Malgun Gothic", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(127)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(184, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(570, 81);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Sunrise Encryption";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Malgun Gothic", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(163)))), ((int)(((byte)(7)))));
+            this.label2.Location = new System.Drawing.Point(189, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(523, 54);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Monarch TSA 2021 - 2022";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(249)))), ((int)(((byte)(250)))));
+            this.panel1.Controls.Add(this.HomePagePanel);
+            this.panel1.Location = new System.Drawing.Point(1, 176);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1261, 660);
+            this.panel1.TabIndex = 3;
+            // 
+            // SidePanel
+            // 
+            this.SidePanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(233)))), ((int)(((byte)(236)))), ((int)(((byte)(239)))));
+            this.SidePanel.Controls.Add(this.button2);
+            this.SidePanel.Controls.Add(this.button1);
+            this.SidePanel.Controls.Add(this.FileEncryptionButton);
+            this.SidePanel.Controls.Add(this.EncryptButton);
+            this.SidePanel.Controls.Add(this.HashButton);
+            this.SidePanel.Controls.Add(this.HomeButton);
+            this.SidePanel.Location = new System.Drawing.Point(1, 176);
+            this.SidePanel.Name = "SidePanel";
+            this.SidePanel.Size = new System.Drawing.Size(300, 657);
+            this.SidePanel.TabIndex = 4;
+            // 
+            // HorizontalDivider
+            // 
+            this.HorizontalDivider.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(127)))), ((int)(((byte)(0)))));
+            this.HorizontalDivider.Location = new System.Drawing.Point(0, 165);
+            this.HorizontalDivider.Name = "HorizontalDivider";
+            this.HorizontalDivider.Size = new System.Drawing.Size(1296, 10);
+            this.HorizontalDivider.TabIndex = 0;
+            // 
+            // HomeButton
+            // 
+            this.HomeButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(226)))), ((int)(((byte)(230)))));
+            this.HomeButton.FlatAppearance.BorderSize = 0;
+            this.HomeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.HomeButton.Font = new System.Drawing.Font("Malgun Gothic", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HomeButton.Location = new System.Drawing.Point(11, 15);
+            this.HomeButton.Name = "HomeButton";
+            this.HomeButton.Size = new System.Drawing.Size(277, 70);
+            this.HomeButton.TabIndex = 0;
+            this.HomeButton.Text = "Home";
+            this.HomeButton.UseVisualStyleBackColor = false;
+            // 
+            // HashButton
+            // 
+            this.HashButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(226)))), ((int)(((byte)(230)))));
+            this.HashButton.FlatAppearance.BorderSize = 0;
+            this.HashButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.HashButton.Font = new System.Drawing.Font("Malgun Gothic", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HashButton.Location = new System.Drawing.Point(11, 115);
+            this.HashButton.Name = "HashButton";
+            this.HashButton.Size = new System.Drawing.Size(277, 70);
+            this.HashButton.TabIndex = 1;
+            this.HashButton.Text = "Hashing";
+            this.HashButton.UseVisualStyleBackColor = false;
+            // 
+            // EncryptButton
+            // 
+            this.EncryptButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(226)))), ((int)(((byte)(230)))));
+            this.EncryptButton.FlatAppearance.BorderSize = 0;
+            this.EncryptButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.EncryptButton.Font = new System.Drawing.Font("Malgun Gothic", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EncryptButton.Location = new System.Drawing.Point(11, 215);
+            this.EncryptButton.Name = "EncryptButton";
+            this.EncryptButton.Size = new System.Drawing.Size(277, 70);
+            this.EncryptButton.TabIndex = 2;
+            this.EncryptButton.Text = "Encryption";
+            this.EncryptButton.UseVisualStyleBackColor = false;
+            // 
+            // VerticalDivider
+            // 
+            this.VerticalDivider.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(127)))), ((int)(((byte)(0)))));
+            this.VerticalDivider.Location = new System.Drawing.Point(295, 176);
+            this.VerticalDivider.Name = "VerticalDivider";
+            this.VerticalDivider.Size = new System.Drawing.Size(10, 660);
+            this.VerticalDivider.TabIndex = 1;
+            // 
+            // FileEncryptionButton
+            // 
+            this.FileEncryptionButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(226)))), ((int)(((byte)(230)))));
+            this.FileEncryptionButton.FlatAppearance.BorderSize = 0;
+            this.FileEncryptionButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.FileEncryptionButton.Font = new System.Drawing.Font("Malgun Gothic", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FileEncryptionButton.Location = new System.Drawing.Point(12, 315);
+            this.FileEncryptionButton.Name = "FileEncryptionButton";
+            this.FileEncryptionButton.Size = new System.Drawing.Size(277, 70);
+            this.FileEncryptionButton.TabIndex = 3;
+            this.FileEncryptionButton.Text = "File Encryption";
+            this.FileEncryptionButton.UseVisualStyleBackColor = false;
+            // 
+            // HomePagePanel
+            // 
+            this.HomePagePanel.Controls.Add(this.HashingPagePanel);
+            this.HomePagePanel.Location = new System.Drawing.Point(306, 3);
+            this.HomePagePanel.Name = "HomePagePanel";
+            this.HomePagePanel.Size = new System.Drawing.Size(955, 657);
+            this.HomePagePanel.TabIndex = 0;
+            // 
+            // HashingPagePanel
+            // 
+            this.HashingPagePanel.Location = new System.Drawing.Point(3, 3);
+            this.HashingPagePanel.Name = "HashingPagePanel";
+            this.HashingPagePanel.Size = new System.Drawing.Size(955, 657);
+            this.HashingPagePanel.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(226)))), ((int)(((byte)(230)))));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Malgun Gothic", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(11, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(277, 70);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Plugins";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(226)))), ((int)(((byte)(230)))));
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Malgun Gothic", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(11, 515);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(277, 70);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Settings";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(144F, 144F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1265, 826);
+            this.Controls.Add(this.VerticalDivider);
+            this.Controls.Add(this.HorizontalDivider);
+            this.Controls.Add(this.SidePanel);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.SidePanel.ResumeLayout(false);
+            this.HomePagePanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel SidePanel;
+        private System.Windows.Forms.Panel HorizontalDivider;
+        private System.Windows.Forms.Button HomeButton;
+        private System.Windows.Forms.Button EncryptButton;
+        private System.Windows.Forms.Button HashButton;
+        private System.Windows.Forms.Panel VerticalDivider;
+        private System.Windows.Forms.Button FileEncryptionButton;
+        private System.Windows.Forms.Panel HashingPagePanel;
+        private System.Windows.Forms.Panel HomePagePanel;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
